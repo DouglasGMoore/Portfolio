0abb8b136017b6b621a32b269d5c17e2247640b8
douglasgmoore <douglasg.moore@gmail.com>
2019-07-02 12:18:55 -0500

    edit grammar

--- components/pages/Welcome.js
+++ components/pages/Welcome.js
@@ -1,7 +1,7 @@
 export default function(state){
     return `    
         <div id = 'banner'>
-        Hello, And welcome to my .redBall Productions!
+        Hello, And welcome to .redBall Productions!
              </div>
         <div id="globe">
         </div>       
